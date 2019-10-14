f90a601d665b9ad90d8fa2a05ba7a12d4c19b146
Peter Tshiani <tshianipeter@gmail.com>
2019-10-14 11:05:01 +0200

    Updates

---
+++ precache-manifest.b0d7af29949876418f27e8ab6fcbf575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f64b30119813e9ad1ea68bbb1dafed",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "08c34d641756eefb11b7",
+    "url": "/weather-app/static/css/main.bbf995b4.chunk.css"
+  },
+  {
+    "revision": "5d34d69a6c09c6c5bd91",
+    "url": "/weather-app/static/js/2.b22e3067.chunk.js"
+  },
+  {
+    "revision": "08c34d641756eefb11b7",
+    "url": "/weather-app/static/js/main.14b70401.chunk.js"
+  },
+  {
+    "revision": "52387862e54d9be38456",
+    "url": "/weather-app/static/js/runtime~main.68dbe4ab.js"
+  },
+  {
+    "revision": "2f9e7eb92962809f53620126f7db8620",
+    "url": "/weather-app/static/media/weather.2f9e7eb9.jpg"
+  }
+]);
